6c6b49bca25280cf69cb1581e20401c0edffb53a
vfadeev <v.fadeev@velvetech.dev>
2023-11-15 09:32:17 +0000

    feat: add web3modal for wallet

--- src/components/UI/baseline/atoms/ConnectButton.tsx
+++ src/components/UI/baseline/atoms/ConnectButton.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Theme, makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import TooltipBasicLayout from '../../../templates/TooltipBasicLayout';
-import { useWeb3Modal } from '@web3modal/ethers5/react'
-
-// import { Web3Auth } from "@web3auth/modal";
-// import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import Web3Modal from 'web3modal';
+import Web3 from 'web3';
 
 const useStyles = makeStyles((theme: Theme) => ({
   connectButton: {
@@ -20,19 +18,45 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }));
 
+
+
 const ConnectButton: React.FC = () => {
   const classes = useStyles();
-  const { open } = useWeb3Modal()
+
+  // code
+const [web3Modal, setWeb3Modal] = useState({});
+const [account, setAccount] = useState<string>("");
+
+  const connectWallet = async () => {
+    try {
+      const web3Modal = new Web3Modal({
+        network: "mainnet", // Optional. Change to desired network
+        cacheProvider: true, // Optional. Use to cache provider
+        providerOptions: {} // Define wallet providers
+      });
+
+      const provider = await web3Modal.connect();
+      const web3 = new Web3(provider);
+
+      const accounts = await web3.eth.getAccounts();
+      if (accounts.length > 0) {
+        setAccount(accounts[0]);
+      }
+    } catch (error) {
+      console.error("Could not connect to wallet:", error);
+    }
+  };
 
   return (
     <TooltipBasicLayout title="Connect">
       <Button
         className={classes.connectButton}
-        onClick={() => open({ view: 'Networks' })}
+        onClick={connectWallet}
         color="secondary"
       >
-        Connect
+        {account ? <p>Connected account: {account}</p> : "Connect"}
       </Button>
+      
     </TooltipBasicLayout>
   )
 }
