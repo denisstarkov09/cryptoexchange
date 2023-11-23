2fbd350946ea30fd0ece0ce6bb11e4ae9f241230
saeid <saeidnobahari1@gmail.com>
2023-11-23 05:01:05 -0800

    fix service issues

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -6,6 +6,7 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
   }
